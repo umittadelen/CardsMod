7cf510cd42b2dc66c06f30a25745e71f25592548
umittadelen <umittadelen1277@gmail.com>
2025-05-21 18:30:19 +0200

    init

---
+++ src/main/java/com/umittadelen/cards/CardsModClient.java
@@ -0,0 +1,10 @@
+package com.umittadelen.cards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CardsModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
---
+++ src/main/java/com/umittadelen/cards/CardsModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.umittadelen.cards;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CardsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
