567f6fa77fee61089f7c6c166afd7061427d7188
umittadelen <umittadelen1277@gmail.com>
2025-05-22 11:44:58 +0200

    uno cards added

---
+++ generate_json_models/app.py
@@ -0,0 +1,66 @@
+import os
+import json
+
+# Card info
+colors = ["red", "blue", "green", "yellow"]
+special_cards = ["block", "reverse"]
+card_types = [f"w{i}" for i in range(10)] + ["wx2"] + special_cards
+
+
+# Base paths
+model_output_dir = "generate_json_models/uno"
+item_model_output_dir = "generate_json_models/items"
+os.makedirs(model_output_dir, exist_ok=True)
+os.makedirs(item_model_output_dir, exist_ok=True)
+
+# Texture mappings for color fronts
+front_textures = {
+    "red": "f_red",
+    "blue": "f_blue",
+    "green": "f_green",
+    "yellow": "f_yellow"
+}
+
+# Generate JSON models
+model_data = []
+for color in colors:
+    for card in card_types:
+        name = f"uno_{color}_{card}"
+
+        model = {
+            "parent": "cards-mod:item/uno_base",
+            "textures": {
+                "front": f"cards-mod:item/uno/{front_textures[color]}",
+                "side": f"cards-mod:item/uno/numbers/{card}",
+                "middle": "#missing",
+                "joker": "#missing"
+            }
+        }
+
+        # Decide where to place the card texture
+        if card in special_cards:
+            model["textures"]["joker"] = f"cards-mod:item/uno/numbers/{card}"
+            model["textures"]["middle"] = f"#missing"
+        else:
+            model["textures"]["middle"] = f"cards-mod:item/uno/numbers/{card}"
+            model["textures"]["joker"] = f"#missing"
+
+        # Item model
+        item_model = {
+            "model": {
+                "type": "minecraft:model",
+                "model": f"cards-mod:item/uno/{name}",
+            }
+        }
+
+        # Write model JSON
+        path = os.path.join(model_output_dir, f"{name}.json")
+        with open(path, "w") as f:
+            json.dump(model, f, indent=4)
+
+        # Write item JSON
+        path = os.path.join(item_model_output_dir, f"{name}.json")
+        with open(path, "w") as f:
+            json.dump(item_model, f, indent=4)
+
+        model_data.append((name, model))
---
+++ src/main/java/com/umittadelen/cards/item/ModItems.java
@@ -0,0 +1,96 @@
+package com.umittadelen.cards.item;
+
+import java.util.function.Function;
+
+import com.umittadelen.cards.CardsMod;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    // Uno Cards ------------------------>
+    private static final String[] UNO_CARD_NAMES = {
+        "uno_blue_block",
+        "uno_blue_reverse",
+        "uno_blue_w0",
+        "uno_blue_w1",
+        "uno_blue_w2",
+        "uno_blue_w3",
+        "uno_blue_w4",
+        "uno_blue_w5",
+        "uno_blue_w6",
+        "uno_blue_w7",
+        "uno_blue_w8",
+        "uno_blue_w9",
+        "uno_blue_wx2",
+        "uno_green_block",
+        "uno_green_reverse",
+        "uno_green_w0",
+        "uno_green_w1",
+        "uno_green_w2",
+        "uno_green_w3",
+        "uno_green_w4",
+        "uno_green_w5",
+        "uno_green_w6",
+        "uno_green_w7",
+        "uno_green_w8",
+        "uno_green_w9",
+        "uno_green_wx2",
+        "uno_red_block",
+        "uno_red_reverse",
+        "uno_red_w0",
+        "uno_red_w1",
+        "uno_red_w2",
+        "uno_red_w3",
+        "uno_red_w4",
+        "uno_red_w5",
+        "uno_red_w6",
+        "uno_red_w7",
+        "uno_red_w8",
+        "uno_red_w9",
+        "uno_red_wx2",
+        "uno_yellow_block",
+        "uno_yellow_reverse",
+        "uno_yellow_w0",
+        "uno_yellow_w1",
+        "uno_yellow_w2",
+        "uno_yellow_w3",
+        "uno_yellow_w4",
+        "uno_yellow_w5",
+        "uno_yellow_w6",
+        "uno_yellow_w7",
+        "uno_yellow_w8",
+        "uno_yellow_w9",
+        "uno_yellow_wx2",
+        "uno_wild_color",
+        "uno_wild_x4",
+        "uno_wild"
+    };
+    public static final Item[] UNO_CARDS = new Item[UNO_CARD_NAMES.length];
+    static {
+        for (int i = 0; i < UNO_CARD_NAMES.length; i++) {
+            UNO_CARDS[i] = registerItem(UNO_CARD_NAMES[i], Item::new, new Item.Settings());
+        }
+    }
+
+    public static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings setting) {
+        final RegistryKey<Item> registerKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(CardsMod.MOD_ID, name));
+        return Items.register(registerKey, factory, setting);
+    }
+
+    private static void UnoCards(FabricItemGroupEntries entries) {
+        for (Item item : UNO_CARDS) {
+            entries.add(item);
+        }
+    }
+
+    public static void registerModItems(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::UnoCards);
+    }
+}
